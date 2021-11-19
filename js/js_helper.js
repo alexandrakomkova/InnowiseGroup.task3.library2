b3cb6c98482341307edcb7f26cfa050879d69e6d
alexandrakomkova <sashakomkova2001@gmail.com>
2021-11-19 19:32:40 +0300

    fix update func, clean up code

--- js/js_helper.js
+++ js/js_helper.js
@@ -8,6 +8,7 @@ function form_instance_url(url, id) {
     let instance_url = url+'/'+id.toString();
     return instance_url;
 }
+
 function set_result_to_div (data, result_div) {
     const array=[data];
     clear_result_div(document.getElementById(result_div))
@@ -17,11 +18,13 @@ function set_result_to_div (data, result_div) {
             document.getElementById(result_div).appendChild(result);
         });
 }
+
 function clear_result_div(div){
     while (div.firstChild) {
         div.removeChild(div.firstChild);
     }
 }
+
 function error_alert(error){
     alert(" Can't do because: " + error.toString());
 }
@@ -33,10 +36,12 @@ function add_author(){
     }
     post(author_url, json_author)
 }
+
 function delete_author() {
     let author_id = document.getElementById('author_id').value.toString();
     delete_instance(form_instance_url(author_url, author_id));
 }
+
 function update_author(){
     let json_author = {
         "name": document.getElementById('author_name').value.toString(),
@@ -45,10 +50,12 @@ function update_author(){
     let author_id = document.getElementById('author_id').value.toString();
     put(form_instance_url(author_url, author_id),json_author);
 }
+
 function get_author(){
     let author_id = document.getElementById('author_id').value.toString();
     get(form_instance_url(author_url, author_id), 'result_author')
 }
+
 function get_all_authors(){
     get(author_url, 'result_author')
 }
@@ -59,10 +66,12 @@ function add_genre(){
     }
     post(genre_url, json_genre)
 }
+
 function delete_genre() {
     let genre_id = document.getElementById('genre_id').value.toString();
     delete_instance(form_instance_url(genre_url, genre_id));
 }
+
 function update_genre(){
     let json_genre = {
         "genre": document.getElementById('genre_name').value.toString()
@@ -70,10 +79,12 @@ function update_genre(){
     let genre_id = document.getElementById('genre_id').value.toString();
     put(form_instance_url(genre_url, genre_id),json_genre);
 }
+
 function get_genre(){
     let genre_id = document.getElementById('genre_id').value.toString();
     get(form_instance_url(genre_url, genre_id),'result_genre')
 }
+
 function get_all_genres(){
     get(genre_url, 'result_genre')
 }
@@ -86,10 +97,12 @@ function add_publisher(){
     }
     post(publisher_url, json_publisher)
 }
+
 function delete_publisher() {
     let publisher_id = document.getElementById('publisher_id').value.toString();
     delete_instance(form_instance_url(publisher_url, publisher_id));
 }
+
 function update_publisher(){
     let json_publisher = {
         "name": document.getElementById('publisher_name').value,
@@ -99,10 +112,12 @@ function update_publisher(){
     let publisher_id = document.getElementById('publisher_id').value.toString();
     put(form_instance_url(publisher_url, publisher_id),json_publisher);
 }
+
 function get_publisher(){
     let publisher_id = document.getElementById('publisher_id').value.toString();
     get(form_instance_url(publisher_url, publisher_id), 'result_publisher')
 }
+
 function get_all_publishers(){
     get(publisher_url, 'result_publisher')
 }
@@ -118,14 +133,17 @@ function add_book(){
     }
     post(book_url, json_book)
 }
+
 function get_book(){
     let book_id = document.getElementById('book_id').value.toString();
     get(form_instance_url(book_url, book_id).trim(), 'result_book')
 }
+
 function delete_book() {
     let book_id = document.getElementById('book_id').value.toString();
     delete_instance(form_instance_url(book_url, book_id));
 }
+
 function update_book(){
     let json_book = {
         "title": document.getElementById('book_title').value.toString(),
@@ -138,6 +156,7 @@ function update_book(){
     let book_id = document.getElementById('book_id').value.toString();
     put(form_instance_url(book_url, book_id),json_book);
 }
+
 function get_all_books(){
     get(book_url, 'result_book')
 }
@@ -148,10 +167,12 @@ function add_format(){
     }
     post(format_url, json_format)
 }
+
 function delete_format() {
     let format_id = document.getElementById('format_id').value.toString();
     delete_instance(form_instance_url(format_url, format_id));
 }
+
 function update_format(){
     let json_format = {
         "format": document.getElementById('format_name').value.toString()
@@ -159,10 +180,12 @@ function update_format(){
     let format_id = document.getElementById('format_id').value.toString();
     put(form_instance_url(format_url, format_id),json_format);
 }
+
 function get_format(){
     let format_id = document.getElementById('format_id').value.toString();
     get(form_instance_url(format_url, format_id), 'result_format')
 }
+
 function get_all_formats(){
     get(format_url, 'result_format')
 }
